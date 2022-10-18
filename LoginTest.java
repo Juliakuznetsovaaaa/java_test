46414e5234d991a28b1dff5c05fed80359278349
Juliakuznetsovaaaa <99537178+Juliakuznetsovaaaa@users.noreply.github.com>
2022-10-18 18:59:41 +0300

    Автотесты_работа1

---
+++ LoginTest.java
@@ -0,0 +1,42 @@
+package com;
+import org.junit.BeforeClass;
+import org.junit.AfterClass;
+
+import org.junit.Test;
+import org.apache.log4j.BasicConfigurator;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.util.concurrent.TimeUnit;
+import org.junit.Assert;
+public class LoginTest{
+public static LoginPage loginPage;
+public static ProfilePage profilePage;
+public static WebDriver driver;
+
+
+    @BeforeClass
+    public static void setup(){
+        //System.setProperty("webdriver.chrome.driver", ConfProperties.getProperty("chromedriver"));
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Julia\\IdeaProjects\\Untitled5\\chromedriver.exe");
+        driver=new ChromeDriver();
+        loginPage=new LoginPage(driver);
+        profilePage = new ProfilePage(driver);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://ok.ru/dk?st.cmd=anonymMain&st.layer.cmd=PopLayerClose");
+    }
+    @Test
+    public void loginTest(){
+        BasicConfigurator.configure();
+
+        loginPage.inputLogin(ConfProperties.getProperty("login"));
+        loginPage.clickLoginBtn();
+        loginPage.inputPasswd(ConfProperties.getProperty("password"));
+        loginPage.clickLoginBtn();
+       /* String user = profilePage.getUserName();
+        Assert.assertEquals(ConfProperties.getProperty("name"), user);*/
+
+    }
+    @AfterClass
+    public static void tearDown() {
+        driver.quit(); }
+}
---
+++ ProfilePage.java
@@ -0,0 +1,29 @@
+package com;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+public class ProfilePage {
+
+    public WebDriver driver;
+    public ProfilePage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+        this.driver = driver;
+    }
+
+    @FindBy(xpath = "//*[@id='hook_Block_TFCounter']")
+    private WebElement userMenu;
+
+    /*@FindBy (xpath = "//*[@id=\"hook_Block_Navigation\"]/div/div/div[1]/a")
+    private WebElement userName;
+
+    public String getUserName() {
+        String name = userName.getText();
+        return name;
+    }*/
+
+    public void entryMenu() {
+        userMenu.click();
+    }
+}
